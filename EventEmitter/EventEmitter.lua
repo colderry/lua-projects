dea469612b55e2750b84af84736c7cc614f54a9c
Colderry <c0l4e6r7@gmail.com>
2022-09-10 01:40:51 +0600

    Add error checking for name and fn params

--- EventEmitter/EventEmitter.lua
+++ EventEmitter/EventEmitter.lua
@@ -3,6 +3,14 @@ EventEmitter = {
 }
 
 function EventEmitter:Subscribe(name, fn)
+	if not(type(name) == "string") then
+		error(string.format("Expected a 'string' in name but got '%s' instead", type(name)))
+	end
+
+	if not(type(fn) == "function") then
+		error(string.format("Expected a 'function' in name but got '%s' instead", type(fn)))
+	end
+
 	if self.listeners[name] then
 		table.insert(
 			self.listeners[name],
@@ -15,6 +23,10 @@ function EventEmitter:Subscribe(name, fn)
 end
 
 function EventEmitter:UnSubscribe(name, pos)
+	if not(type(name) == "string") then
+		error(string.format("Expected a 'string' in name but got '%s' instead", type(name)))
+	end
+
 	if self.listeners[name] then
 		if pos == nil then
 			self.listeners[name] = nil
@@ -25,6 +37,10 @@ function EventEmitter:UnSubscribe(name, pos)
 end
 
 function EventEmitter:Fire(name, ...)
+	if not(type(name) == "string") then
+		error(string.format("Expected a 'string' in name but got '%s' instead", type(name)))
+	end
+
 	if not(self.listeners[name]) then
 		self.listeners[name] = {}
 	end
